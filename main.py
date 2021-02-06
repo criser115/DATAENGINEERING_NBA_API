64ed7b3cf72771a8c51e5d4011a776efe3000bd7
criser115 <criser115@gmail.com>
2021-02-05 23:28:49 -0600

    Add code to extract NBA data

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+import pandas as pd
+import requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+DATABASE_LOCATION = "sqlite:///nba_player_stats.sqlite"
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+if __name__ == "__main__":
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+
+    r = requests.get("https://www.balldontlie.io/api/v1/stats", headers=headers)
+
+    data = r.json()
+
+    print(data)

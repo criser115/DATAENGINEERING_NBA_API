fcefbe05dc09a2d12a6533c6bcde40d64c49e83f
criser115 <criser115@gmail.com>
2021-02-09 19:12:02 -0600

    Created a dataframe to list the top 10 scorers in a single NBA game

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import requests
 
+
 DATABASE_LOCATION = "sqlite:///nba_player_stats.sqlite"
 
 
@@ -11,8 +12,31 @@ if __name__ == "__main__":
         "Content-Type": "application/json"
     }
 
-    r = requests.get("https://www.balldontlie.io/api/v1/stats", headers=headers)
+    r = requests.get("https://www.balldontlie.io/api/v1/stats?seasons[]=2020", headers=headers)
 
     data = r.json()
+    #print(data)
 
-    print(data)
+    players = []
+    teams = []
+    pts = []
+    game_id = []
+
+    for item in data["data"]:
+        players.append(item["player"]["first_name"] + " " + item["player"]["last_name"])
+        teams.append(item["team"]["full_name"])
+        pts.append(item["pts"])
+        game_id.append(item["game"]["id"])
+
+    nba_dict = {
+        "NBA_Players": players,
+        "NBA_Teams": teams,
+        "Points_Scored": pts,
+        "Game_ID": game_id
+    }
+
+    nba_df = pd.DataFrame(nba_dict, columns=["NBA_Players", "NBA_Teams", "Points_Scored", "Game_ID"])
+    print(nba_df.nlargest(10, columns=['Points_Scored']))
+
+
+
